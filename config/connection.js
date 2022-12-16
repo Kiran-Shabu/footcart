006347b0c2feb2d1d445cc9187f7b039fd710a3b
kiranshabu <kiranshabu@gmail.com>
2022-12-16 20:23:45 +0530

    second commit

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=function(done){
-    const url = 'mongodb://localhost:27017';
+    const url = 'mongodb+srv://kiranshabu:kiran123@cluster0.ozmtqu4.mongodb.net/test';
     const dbname='footkart'
 
     MongoClient.connect(url,(err,data)=>{
